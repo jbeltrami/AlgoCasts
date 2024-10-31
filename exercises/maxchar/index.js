e1712cc3f3a1525bd017a96c82389f96666accd2
jBeltrami <joaopbeltrami@gmail.com>
2024-10-31 12:03:56 -0300

    Resolve maxChar
    This solution splits and reduces a string, to get a count of each individual character in an object.
    Then the object entries get reduced until only the pair with the highest count is left. The key of said pair is then returned.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const objects = str.split("").reduce((acc, char) => {
+    acc[char] = acc[char] + 1 || 1;
+
+    return acc;
+  }, {});
+
+  const highest = Object.entries(objects).reduce(
+    (acc, [key, value]) => {
+      return value > acc[1] ? [key, value] : acc;
+    },
+    ["", 0]
+  );
+
+  return highest[0];
+}
+
+maxChar("abcccccccd");
 
 module.exports = maxChar;
