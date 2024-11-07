9c0319327736dcaec8882d92ce52e2ee859ffafa
jBeltrami <joaopbeltrami@gmail.com>
2024-11-07 11:41:22 -0300

    Solve anagram using a character map
    This solution was provided by the course's instructor.
    It's being saved in this repo for future reference

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,10 +9,39 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const normalizeString = (str) =>
-    str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join();
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
 
-  return normalizeString(stringA) === normalizeString(stringB);
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
 }
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const stringFormat = /[^\w]/g;
+
+// const normalizeString = (str) =>
+//   str.replace(stringFormat, "").toLowerCase().split("").sort().join();
+
+// return normalizeString(stringA) === normalizeString(stringB);
+// }
