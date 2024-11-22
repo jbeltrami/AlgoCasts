2d00990d1a2b547227b4c9e5ec2f85f6299cd081
jBeltrami <joaopbeltrami@gmail.com>
2024-11-22 10:50:31 -0300

    Solve pyramid with iterative solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // From 0 to n
+  // Create an empty string, 'level'
+  // 	From 0 to ?? (cols)
+  // 		if the column should have a #
+  // 			Add a # to level
+  // 		else
+  // 			Add ' '
+  // 	console.log(stair)
+
+  const cols = n * 2 - 1;
+  const mid = Math.floor(cols / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let col = 0; col < cols; col++) {
+      // Determine how many empty spaces we need per col: cols - currentRow
+      mid - row <= col && mid + row >= col ? (level += "#") : (level += " ");
+    }
+
+    console.log(level);
+  }
+}
+
+// pyramid(1);
+// pyramid(2);
+pyramid(3);
 
 module.exports = pyramid;
