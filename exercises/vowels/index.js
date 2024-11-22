67c0ea29f98ac91f8ab5c37618b2a8677d2cde6c
jBeltrami <joaopbeltrami@gmail.com>
2024-11-22 11:23:08 -0300

    Solve vowels with array reducer

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  return str.split("").reduce((acc, el, idx) => {
+    return vowels.includes(el.toLowerCase()) ? acc + 1 : acc;
+  }, 0);
+}
+
+vowels("Why do you ask?");
 
 module.exports = vowels;
