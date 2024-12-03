51687905eb1908960f95b7f58b87390cb698c1d3
jBeltrami <joaopbeltrami@gmail.com>
2024-12-03 12:22:13 -0300

    Solve matrix spiral number generator
    Adds solution for generating n x n matrices filled with consecutive numbers in a spiral pattern from outside to inside. Uses a counter with four directional phases:
    - Left to right
    - Top to bottom
    - Right to left
    - Bottom to top

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,64 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  // matrix length = n**2
+  // number of rows = n
+  // row length = n
+  const results = [];
+
+  for (let row = 0; row < n; row++) {
+    results.push([]);
+  }
+
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Run left to right
+    for (let toRight = 0; toRight <= endColumn; toRight++) {
+      results[startRow][toRight] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // Run top to bottom
+    for (let toBottom = startRow; toBottom <= endRow; toBottom++) {
+      results[toBottom][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    // Run right to left
+    for (let toLeft = endColumn; toLeft >= startColumn; toLeft--) {
+      results[endRow][toLeft] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // Run bottom to top
+    for (let toUp = endRow; toUp > startRow; toUp--) {
+      results[toUp][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+  console.log(
+    { counter },
+    { startColumn },
+    { endColumn },
+    { startRow },
+    { endRow }
+  );
+
+  console.log(results);
+  return results;
+}
+
+matrix(4);
 
 module.exports = matrix;
